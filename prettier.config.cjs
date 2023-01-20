65244454adb04489ca2f0500b1d57f88895209ba
Ryan Arora <arora.ryan@gmail.com>
2023-01-19 19:56:18 -0500

    add prettier plugins + prettier format

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,17 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  plugins: [
+    require.resolve("prettier-plugin-tailwindcss"),
+    require.resolve("prettier-plugin-organize-imports"),
+    require.resolve("prettier-plugin-prisma"),
+  ],
+  overrides: [
+    {
+      files: "*.prisma",
+      options: {
+        parser: "prisma-parse",
+        tabWidth: 4,
+      },
+    },
+  ],
 };
